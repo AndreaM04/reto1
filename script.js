a346625e27f03f640bd0d0390a9dfea4150d6ed8
AndreaM04 <146774480+AndreaM04@users.noreply.github.com>
2023-10-20 17:53:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function name() {
-    let n1 = document.getElementById('cuenta').value
+function empanadas() {
+    
+var uno = document.getElementById("cuenta").value
+var dos = document.getElementById("porce").value
+
+var tres = dos * uno
+tres = tres / 100
+cuatro = Number(uno) + Number(tres)
+var iuno = document.getElementById("tCuenta")
+var idos = document.getElementById("propinas")
+var itres = document.getElementById("tpagar")
+
+iuno.innerText = (uno)
+idos.innerText = tres
+itres.innerText = cuatro
 }
+
+
